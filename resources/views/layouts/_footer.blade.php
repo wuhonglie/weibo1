a43763a3635fd5fc4254291d45dff32890903371
wuhonglie <2429986613@qq.com>
2019-05-06 02:13:45 +0000

    命名路由

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -2,6 +2,6 @@
   <img src="https://iocaffcdn.phphub.org/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png" alt="" class="brand-icon">
   <a href="https://learnku.com/laravel/courses" target="_blank">刻意练习,每日精进</a>
   <div class="float-right">
-    <a href="/about">关于</a>
+    <a href="{{ route('about') }}">关于</a>
   </div>
 </footer>
