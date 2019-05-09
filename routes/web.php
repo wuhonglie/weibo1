57629661f8e086b45b523facbe22483a5f9a7061
wuhonglie <2429986613@qq.com>
2019-05-09 01:50:38 +0000

    用户激活

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,12 @@
 |
 */
 
-Route::get('/','StaticPagesController@home')->name('home');
-Route::get('help','StaticPagesController@help')->name('help');
-Route::get('about','StaticPagesController@about')->name('about');
-Route::get('signup','UsersController@create')->name('signup');
-Route::resource('users','UsersController');
-Route::get('login','SessionsController@create')->name('login');
-Route::post('login','SessionsController@store')->name('login');
-Route::delete('logout','SessionsController@logout')->name('logout');
+Route::get('/', 'StaticPagesController@home')->name('home');
+Route::get('help', 'StaticPagesController@help')->name('help');
+Route::get('about', 'StaticPagesController@about')->name('about');
+Route::get('signup', 'UsersController@create')->name('signup');
+Route::resource('users', 'UsersController');
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@logout')->name('logout');
+Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
