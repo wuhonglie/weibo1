b9bd27f14bd8eb1c1a455eb877ec4cbfed2bccf9
wuhonglie <2429986613@qq.com>
2019-05-10 01:21:38 +0000

    发布微博

---
+++ resources/views/shared/_status_form.blade.php
@@ -0,0 +1,8 @@
+<form action="{{ route('statuses.store') }}" method="post">
+  @include('shared._errors')
+  {{ csrf_field() }}
+  <textarea name="content" rows="3" class="form-control" placeholder="聊聊新鲜事儿...">{{ old('content') }}</textarea>
+  <div class="text-right">
+    <button type="submit" class="btn btn-primary mt-3">发布</button>
+  </div>
+</form>
